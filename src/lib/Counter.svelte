f43b46139e6e87808dba7bdf3d864b545c6a3a69
justinenglish <justnenglish@gmail.com>
2022-12-28 16:02:46 -0500

    Initial app setup

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+  const reset = () => {
+    count = 0
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+<button on:click={reset}>
+  Reset counter
+</button>
